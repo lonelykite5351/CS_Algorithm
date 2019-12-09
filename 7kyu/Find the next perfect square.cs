5329c97d8a2742d52508ddec0355046f5346e0ab
lonelykite5351 <lonelykite5351@gmail.com>
2019-12-10 00:49:26 +0800

    2019/12/10

---
+++ 7kyu/Find the next perfect square.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CS_Algorithm._7kyu
+{
+    class Find_the_next_perfect_square
+    {
+        //public static long FindNextSquare(long num)
+        //{
+        //    // your code here
+        //}
+
+    }
+}
---
+++ EntryPoint.cs
@@ -0,0 +1,25 @@
+﻿using CS_Algorithm._6kyu;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Algorithm
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+
+            int FindEvenIndex1 = Equal_Sides_Of_An_Array
+                .FindEvenIndex(new int[] { 1, 2, 3, 4, 3, 2, 1 });
+            int FindEvenIndex2 = Equal_Sides_Of_An_Array
+                .FindEvenIndex(new int[] { 1, 100, 50, -51, 1, 1 });
+
+
+            Console.WriteLine("hello");
+            Console.WriteLine(FindEvenIndex1);
+            Console.WriteLine(FindEvenIndex2);
+            Console.Read();
+        }
+    }
+}
